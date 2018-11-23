d70cd57cac7ce9982b608366af0cb1aad08e1cdd
michaelpham6897 <mp1317@scarletmail.rutgers.edu>
2018-11-22 20:53:31 -0500

    Python Project
    The bot will randomly mix and match given inputs, in a form of a string, to form a new lyric, or string.

---
+++ Python Project.py
@@ -0,0 +1,20 @@
+import random #Allows me to randomly match the two lists
+
+L = ["Sunshine she's here,  you can take a break", "I think about you day and night,  it's only right", "When you're with me,  baby the skies'll be blue"]
+"""
+Songs: Happy by Pharrell Williams, Happy Together by The Turtles,
+"""
+M = ["Whether skies are gray or blue  any place on earth will do", ""]
+"""
+Songs: My Happiness by Connie Francis,
+"""
+
+def mix_match(L, M):
+    """
+    This function will randomly match a string from list L with a string from list M
+
+    L: A list of strings
+    M: A list of strings
+    """
+    L.split("  ")
+    M.split("  ")
