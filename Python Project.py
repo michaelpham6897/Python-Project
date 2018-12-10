e3e3fb63a789f02347252c00f36d025013fcf35c
michaelpham6897 <mp1317@scarletmail.rutgers.edu>
2018-12-10 17:45:23 -0500

    Project

--- Python Project.py
+++ Python Project.py
@@ -1,20 +1,42 @@
 import random #Allows me to randomly match the two lists
+import threading #Allows bot to periodically print a new string
 
-L = ["Sunshine she's here,  you can take a break", "I think about you day and night,  it's only right", "When you're with me,  baby the skies'll be blue"]
+L = ["Sunshine she's here,", "I think about you day and night,", "When you're with me,", "But you heard it, darling,", "Fighting against all odds", "Whether skies are gray or blue", "Or stay up on the phone late at night when you're alone,", "I'll give the shelter,"]
+M = ["you can take a break", "it's only right", "baby the skies'll be blue", "you look perfect tonight", "I know we'll be alright this time", "any place on earth will do", "looking out the window gazing up at stars", "like you've done for me"]
 """
-Songs: Happy by Pharrell Williams, Happy Together by The Turtles,
-"""
-M = ["Whether skies are gray or blue  any place on earth will do", ""]
-"""
-Songs: My Happiness by Connie Francis,
+Songs: Happy by Pharrell Williams, Happy Together by The Turtles, Perfect by Ed Sheeran, My Happiness by Connie Francis, If You Wonder by Jeff Bernat, Shelter by Porter Robinson and Madeon
 """
 
 def mix_match(L, M):
     """
     This function will randomly match a string from list L with a string from list M
 
-    L: A list of strings
-    M: A list of strings
+    L: A list of the first half of a lyric
+    M: A list of the second half of a lyric
     """
-    L.split("  ")
-    M.split("  ")
+    print(random.choice(L) + " " + random.choice(M))
+
+class mix_match_timer(threading.Thread):
+    def __init__(self):
+        threading.Thread.__init__(self)
+        self.__finish = threading.Event()
+        self.__interval = 972.0
+        """
+        I chose this number because an average of 16.2 million adults in the Unites States have depression
+        I converted that number to seconds, then I divided it by 1 million
+        https://www.verywellmind.com/depression-statistics-everyone-should-know-4159056
+        """
+
+    def setInterval(self, interval):
+        self.__interval = __interval
+
+    def shutdown(self):
+        self.__finished.set()
+
+    def run(self):
+        while l:
+            if self.__finished.isSet():
+                return self.task()
+
+    def task(self):
+        pass
---
+++ README.txt
@@ -0,0 +1,11 @@
+Michael Pham
+This is my Python Project. I wanted my project to be on a random lyric mix and match because I listen to music most of the time whenever I am doing something by myself. I also
+chose to only use happy lyrics because I wanted to express that there is something out there that can help someone feel happier in times of need, whether it be a person or an
+inanimate object or even a bot.
+
+I had some struggles getting this program to run. Some problems that I ran into was finding out to to randomly match the lists, and how to set a 
+time interval for when the program should produce another mix and match. Another problem that I encountered was when I tried to split a list, but I can only split a string.
+Therefore, I had to make a list containing the first half of lyrics that I wanted to randomly mix with the second half of another lyric. 
+
+My time interval for when the bot produces another lyric is once every 972 seconds. I chose this number because I found that an average of 16.2 million adults in the Unites
+States have depression. I multiplied that number by 60 then divided it by 1 million because I wanted to represent every 1 million people as a minute.
